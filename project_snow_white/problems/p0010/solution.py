d6d1543d469dbcb152130ef6e69ec1a04678106f
Project Snow White <82291597+psw42@users.noreply.github.com>
2021-04-11 00:18:25 +0200

    Update solution.py

--- project_snow_white/problems/p0010/solution.py
+++ project_snow_white/problems/p0010/solution.py
@@ -5,7 +5,7 @@ def get_num_attempts() -> int:
     seed(24896725)
     i = 0
     while True:
-        flips = (choice([1, 2]), choice([1, 2]))
+        flips = choice([1, 2]), choice([1, 2])
         i += 1
         if flips != (2, 2):
             return i * 2
