44240f89c0d6d4c434342aa3559aaf092b5ae5f3
Project Snow White <82291597+psw42@users.noreply.github.com>
2021-04-11 00:13:55 +0200

    Create solution.py

---
+++ project_snow_white/problems/p0010/solution.py
@@ -0,0 +1,11 @@
+from random import seed, choice
+
+
+def get_num_attempts():
+    seed(24896725)
+    i = 0
+    while True:
+        flips = (choice([1, 2]), choice([1, 2]))
+        i += 1
+        if flips != (2, 2):
+            return i
